31202663f63dd00d7a91bec168cf8d734f440fd1
Suharjin <suharjin01@gmail.com>
2025-02-28 22:48:45 +0800

    Struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct UserLoginRequest {
+    username: String,
+    password: String
+}
+
+
+#[test]
+fn test_create_json_for_user_login_request() {
+    let login_request = UserLoginRequest {
+        username: "testuser".to_string(),
+        password: "rahasia".to_string()
+    };
+
+    let json = serde_json::to_string(&login_request).unwrap();
+    println!("{}", json);
+
+    let login_result: UserLoginRequest = serde_json::from_str(&json).unwrap();
+    println!("{:?}", login_result)
 }
