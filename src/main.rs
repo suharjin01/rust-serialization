3b9209c1986f9f4f6ad801e8e23b8f2243b279b5
Suharjin <suharjin01@gmail.com>
2025-03-01 11:38:23 +0800

    Nested Struct

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,23 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 struct UserLoginRequest {
     username: String,
-    password: String
+    password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct AddressRequest {
+    street: String,
+    city: String,
+    state: String,
+    zip: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CreateUserRequest {
+    username: String,
+    password: String,
+    email: String,
+    address: AddressRequest,
 }
 
 
@@ -19,4 +35,26 @@ fn test_create_json_for_user_login_request() {
 
     let login_result: UserLoginRequest = serde_json::from_str(&json).unwrap();
     println!("{:?}", login_result)
+}
+
+
+#[test]
+fn test_create_json_for_user_create_user_request() {
+    let request = CreateUserRequest {
+        username: "testuser".to_string(),
+        password: "rahasia".to_string(),
+        email: "test03@gmail.com".to_string(),
+        address: AddressRequest {
+            street: "Jalan Kayu".to_string(),
+            city: "Malang".to_string(),
+            state: "Konoha".to_string(),
+            zip: "1313131".to_string()
+        }
+    };
+
+    let json = serde_json::to_string(&request).unwrap();
+    println!("{}", json);
+
+    let result: CreateUserRequest = serde_json::from_str(&json).unwrap();
+    println!("{:?}", result)
 }
